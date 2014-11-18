47a2454e9283134f8b63a655bfeedcdfe386a440
robinbach <bestname@umich.edu>
2014-11-17 22:26:35 -0500

    updating logic

--- doodledrop/GamingWindow.java
+++ doodledrop/GamingWindow.java
@@ -8,9 +8,18 @@ import javax.swing.*;
 
 public class GamingWindow extends JFrame
 {
+  /**
+   * for safety
+   */
+  private static final long serialVersionUID = 1L;
+  
+  public static final int stageLength = 100;
+  public static final int stageWidth = 100;
+
 
   
-  
+  static JLabel position;
+
   public class KeyboardListener extends KeyAdapter
   {
     public void keyPressed(KeyEvent event)
@@ -18,15 +27,14 @@ public class GamingWindow extends JFrame
       System.out.println("pressed");
 
       GameLogic.keyPressed(event.getKeyCode());
+      
       if( event.getKeyCode() == KeyEvent.VK_RIGHT )
       {
         System.out.println("right pressed");
-//        player1.set_velocity(1, 0);
       }
       else if( event.getKeyCode() == KeyEvent.VK_LEFT )
       {
         System.out.println("left pressed");
-//        player1.set_velocity(-1, 0);
       }
       else if( event.getKeyCode() == KeyEvent.VK_Q )
       {
@@ -34,24 +42,44 @@ public class GamingWindow extends JFrame
         GamingWindow.this.dispose();
       }
     }
-  }
+    
+    public void keyReleased(KeyEvent event)
+    {
+      System.out.println("released");
+
+      GameLogic.keyReleased(event.getKeyCode());
+      
+      if( event.getKeyCode() == KeyEvent.VK_RIGHT )
+      {
+        System.out.println("right released");
+//        player1.set_velocity(1, 0);
+      }
+      else if( event.getKeyCode() == KeyEvent.VK_LEFT )
+      {
+        System.out.println("left released");
+//        player1.set_velocity(-1, 0);
+      }
+      else if( event.getKeyCode() == KeyEvent.VK_Q )
+      {
+        System.out.println("Quit released");
+        GamingWindow.this.dispose();
+      }
+    }  }
   
   
   public GamingWindow()
   {
     super("Doodle _(:3Z)_ Drop");
     
-    setMinimumSize(new Dimension(120, 60));
-    pack();
+    setMinimumSize(new Dimension(300, 160));
+    position = new JLabel();
+    add(position);
     setVisible(true);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     
     addKeyListener(new KeyboardListener());
     
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-    
-
 
   }
 }
